2cbf7ec8e1ab150da2a88563ed8ecf94b83c0df7
David Kutalek <davkutalek@gmail.com>
2014-09-23 14:07:34 +0300

    Added edit functionality. Mostly working, but a few details left to work out.

--- drone/js/droneApp.js
+++ drone/js/droneApp.js
@@ -1,6 +1,4 @@
-$(function()
-{
-
+$(function() {
     var cloudmade = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     });
@@ -9,64 +7,62 @@ $(function()
     map.addControl(osmGeocoder);
     map.addControl( L.control.zoom({position: 'bottomright'}) );
 
-    var markers = new L.MarkerClusterGroup({maxClusterRadius:1});
+    var markers = new L.MarkerClusterGroup({ maxClusterRadius: 1 });
     $("#commentDialog").dialog({ autoOpen: false, modal: true, buttons: [ { text: "Cancel", click: function() { $(this).dialog("close"); } }, { text: "Submit", click: submitFlag } ] });
     $("#deleteDialog").dialog({ autoOpen: false, modal: true, buttons: [ { text: "Cancel", click: function() { $(this).dialog("close"); } }, { text: "Submit", click: submitDelete } ] });
     $(".ui-icon").text("X");
     var listRetrieved = false;
-    var openVidId = null;
-    var openVidEmail = null;
+    var videoList = [];
+    var activeVideo = {}
 /**
     var requestURL = 'http://localhost:8081/AIDRTrainerAPI/rest/drone/jsonp/getdrones';
-                        $.ajax({
-                                type: 'GET',
-                                url: requestURL,
-                                dataType: 'jsonp',
-                                success: renderList,
-                                error: FailedRenderList,
-                                jsonp: false,
-                                jsonpCallback: "jsonp"
-                            });
-
+    $.ajax({
+            type: 'GET',
+            url: requestURL,
+            dataType: 'jsonp',
+            success: renderList,
+            error: FailedRenderList,
+            jsonp: false,
+            jsonpCallback: "jsonp"
+        });
 **/
+    var autoGeoRefresh = setInterval(function() { // Call out to get the time
+        var requestURL = "http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/jsonp/getdrones" //'http://gis.micromappers.org/drone/rest/web/jsonp/getdrones';
+        if (typeof indexID != 'undefined')
+            requestURL = 'http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/jsonp/drones/after/' + indexID; //'http://gis.micromappers.org/drone/rest/web/jsonp/drones/after/'
 
-    var autoGeoRefresh = setInterval(
-                function ()  // Call out to get the time
-                {
-                    var requestURL = "http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/jsonp/getdrones" //'http://gis.micromappers.org/drone/rest/web/jsonp/getdrones';
-                    if(typeof indexID != 'undefined' ){
-                        requestURL = 'http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/jsonp/drones/after/' + indexID; //'http://gis.micromappers.org/drone/rest/web/jsonp/drones/after/'
-                    }
-
-                    $.ajax({
-                            type: 'GET',
-                            url: requestURL,
-                            dataType: 'jsonp',
-                            timeout: 4000,
-                            success: renderList,
-                            error: failedRenderList,
-                            jsonp: false,
-                            jsonpCallback: "jsonp",
-                        });
-
-
-                }, 5000);// end check
+        $.ajax({
+            type: 'GET',
+            url: requestURL,
+            dataType: 'jsonp',
+            timeout: 4000,
+            success: renderList,
+            error: failedRenderList,
+            jsonp: false,
+            jsonpCallback: "jsonp",
+        });
+    }, 5000);// end check
 
     function renderList(data) {
         var dataCount = 0;
         listRetrieved = true;
         $("#loading-gif").remove();
         $("#loading-failure").remove();
-        $.each(data, function(i, field){
-            // alert(field.info.email);
-            field.info.email = 'david@spatialcollective.com';
 
+        $.each(data, function(i, field) {
+            video = {
+            	vidId: field.info.id, 
+            	email: null, 
+            	url: field.info.url, 
+            	name: field.info.displayName, 
+            	lat: field.features.geometry.coordinates[1], 
+            	lng: field.features.geometry.coordinates[0] }
+            videoList.push(video);
+            mapDataCollection.push(field);
             dataCount++;
-            geoDatColection.push( field );
-            mapDataCollection = geoDatColection;
         });
 
-        if(dataCount > 0){
+        if (dataCount > 0) {
             displayAllRow(data);
             populateMakers();
             map.addLayer(markers);
@@ -75,21 +71,26 @@ $(function()
     }
 
     function populateMakers(){
-        for( var i in mapDataCollection){
+        for (var i in mapDataCollection) {
             var item = mapDataCollection[i];
-            if(!checkDuplicateEntry(item.info.url)){
+            if (!checkDuplicateEntry(item.info.url)){
                  var layName = L.geoJson(item.features, {
                     onEachFeature: function (features, layer) {
                         indexID = item.info.id;
 
-                        layer.on("click", function (e){
-                            var src = getSrcUrl(getSelectedLayerURL(layer));
+                        layer.on("click", function (e) {
+                        	var url = getSelectedLayerURL(layer);
+                            var src = getSrcUrl(url);
                             $("#uavVideo").attr('src', src);
                             window.location.href='#uavOpenModal';
-                            openVidId = getSelectedLayerID(layer);
-                            openVidEmail = getSelectedLayerEmail(layer);
+                            activeVideo.vidId = getSelectedLayerID(layer);
+                            activeVideo.name = getSelectedLayerName(layer);
+                            activeVideo.email = getSelectedLayerEmail(layer);
+                            activeVideo.url = url;
+                            activeVideo.lng = getSelectedLayerCoordinates(layer)[0];
+                            activeVideo.lat = getSelectedLayerCoordinates(layer)[1];
                         });
-                        geoLayerCollection.push( new layerInfo(layer, item.info.url, item.info.id, item.info.email) ) ;
+                        geoLayerCollection.push( new layerInfo(layer, item.info.url, item.info.displayName, item.info.id, item.info.email, item.features.geometry.coordinates) ) ;
                     }
                 });
                 markers.addLayer(layName);
@@ -102,20 +103,28 @@ $(function()
 
             if(!checkDuplicateEntry(field.info.url)){
                 var displayTxt = '<a href="#uavOpenModal">';
-                displayTxt =  displayTxt + '<p><b>'+ field.info.displayName +'</b></p>';
+                displayTxt =  displayTxt + '<p class="displayName"><b>'+ field.info.displayName +'</b></p>';
                 displayTxt =  displayTxt + '<p>'+ field.info.created +'</p></a>';
 
-                var hiddenURL = '<input name = "uavVideURL" id="uavVideURL" email="' + field.info.email + '" type="hidden" value = "'+ field.info.url +'"/>';
-                displayTxt = displayTxt + hiddenURL;
-                $( "#tweetList" ).prepend($("<li class='ui-widget-content' name='" + field.info.id + "'></li>").html(displayTxt));
+                var hiddenURL = '<input name="uavVideURL" class="uavVideURL" email="' + field.info.email + '" type="hidden" value = "'+ field.info.url +'"/>';
+                var hiddenLat = '<input name="uavCoords" class="uavLat" type="hidden" value="'+ field.features.geometry.coordinates[1] +'"/>';
+                var hiddenLng = '<input name="uavCoords" class="uavLng" type="hidden" value="'+ field.features.geometry.coordinates[0] +'"/>';
+                var liContent = displayTxt + hiddenURL + hiddenLat + hiddenLng;
+                $( "#tweetList" ).prepend($("<li class='ui-widget-content' name='" + field.info.id + "'></li>").html(liContent));
                 $("#tweetList li").unbind("click").click(function(evt) {
                     // console.log("clicker!!!");
                     // var answer = $(evt.currentTarget).text();
                     // var answer2 =$(evt.currentTarget).children('#uavVideURL');
-                    var src = getSrcUrl($(evt.currentTarget).children('#uavVideURL').attr('value'));
+                    var url = $(evt.currentTarget).children('.uavVideURL').attr('value');
+                    var src = getSrcUrl(url);
                     $("#uavVideo").attr('src', src);
-                    openVidId = $(evt.currentTarget).attr('name');
-                    openVidEmail = $(evt.currentTarget).children('#uavVideURL').attr('email');
+                    activeVideo.vidId = $(evt.currentTarget).attr('name');
+                    activeVideo.name = $(evt.currentTarget).find('.displayName').text();
+                    activeVideo.lng = $(evt.currentTarget).find('.uavLng').val();
+                    alert(activeVideo.lng);
+                    activeVideo.lat = $(evt.currentTarget).find('.uavLat').val();
+                    activeVideo.email = $(evt.currentTarget).children('.uavVideURL').attr('email');
+                    activeVideo.url = url;
                     //var vid =  $(this).attr("uavVideURL");
                     // console.log($(evt.currentTarget));
                     // console.log(this);
@@ -147,14 +156,6 @@ $(function()
             e.preventDefault();
             var email = $("input#email").val();
             var vURL = $("input#vURL").val();
-            //console.log(email);
-            //console.log(vURL);
-            //console.log(geoInfoProperty);
-           //$('#submit').text('Request has been sent. Thank you!');
-           // $('input[type="submit"]').attr('disabled','disabled');
-           // alert(geoInfoProperty);
-           // $("#testForm").submit();
-
             var info = new Object();
             info.email = email;
             info.url = vURL;
@@ -162,26 +163,60 @@ $(function()
             var data = new Object();
             data.features = geoInfoProperty;
             data.info = info;
+            if ($("input#vId").length)
+          		data.id = $("input#vId").val();
 
             var myString = JSON.stringify(data);
-           // myString = "jsonp(" + myString + ");"
-           // console.log("myString: " + myString);
-            var urlPost = "http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/add";
-
+            var urlPost = $("#uavInfo").attr('action');
 
             $.post( urlPost, myString );
-            //$('#submit').text('Request has been sent. Thank you!');
-            //$('#submit').attr('disabled','disabled');
             window.location.href='#close';
         }
     });
 
     $("#flag-btn").click(reportVideo);
+    $("#edit-btn").click(editVideo);
     $("#delete-btn").click(deleteVideoConfirm);
 
     function reportVideo() {
         $("#commentDialog .error").remove();
         $("#commentDialog").dialog("open");
+    }
+
+    function submitFlag() {
+        var comment = $("#commentDialog #flagComment").val();
+        if (comment == "" || comment == undefined) {
+            $("#commentDialog label").after("<div class='error'>You must add a comment</div>");
+        } else {
+            jsonData = JSON.stringify({ "id": activeVideo.vidId, "comment": comment });
+            $.post("http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/report/post", jsonData);
+            $("#commentDialog").dialog("close");
+            $("#commentDialog #flagComment").val("");
+        }
+    }
+
+    function editVideo() {
+    	$("#uavVideo").attr('src', '');
+    	window.location.href='#close';
+
+    	window.location.href='#openModal';
+    	$("#openModal #locationRef").val(activeVideo.name);
+    	$("#openModal #vURL").val(activeVideo.url);
+    	$("#openModal #lon").text(activeVideo.lng);
+    	$("#openModal #lat").text(activeVideo.lat);
+
+    	damageLayer.removeAllFeatures();
+    	var lonLat = new OpenLayers.LonLat(activeVideo.lng, activeVideo.lat)
+            .transform(
+                new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+                tinyMap.getProjectionObject() // to Spherical Mercator Projection
+            );
+    	point = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
+        damageLayer.addFeatures([new OpenLayers.Feature.Vector(point)]);
+        tinyMap.setCenter(lonLat,5);
+
+        $("#uavInfo").append("<input id='vId' type='hidden' value='" + activeVideo.vidId + "'></input>")
+        $("#uavInfo").attr('action', "http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/update");
     }
 
     function deleteVideoConfirm() {
@@ -192,27 +227,15 @@ $(function()
         $("#deleteDialog").dialog("open");
     }
 
-    function submitFlag() {
-        var comment = $("#commentDialog #flagComment").val();
-        if (comment == "" || comment == undefined) {
-            $("#commentDialog label").after("<div class='error'>You must add a comment</div>");
-        } else {
-            jsonData = JSON.stringify({ "id": openVidId, "comment": comment });
-            $.post("http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/report/post", jsonData);
-            $("#commentDialog").dialog("close");
-            $("#commentDialog #flagComment").val("");
-        }
-    }
-
     function submitDelete() {
         var email = $("#deleteDialog input").val();
         if (true) { // email == openVidEmail
-            jsonData = JSON.stringify({ "id": openVidId, "email": email });
-            $.post("http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/jsonp/delete/" + openVidId + "/" + email, jsonData, function(data, response) {
+            jsonData = JSON.stringify({ "id": activeVideo.vidId, "email": email });
+            $.post("http://qcricl1linuxvm2.cloudapp.net:8081/AIDRDRONE/rest/web/jsonp/delete/" + activeVideo.vidId + "/" + email, jsonData, function(data, response) {
                 alert(response);
             });
             $("#deleteDialog").dialog("close");
-            $("#tweetList li[name=" + openVidId + "]").remove();
+            $("#tweetList li[name=" + activeVideo.vidId + "]").remove();
             window.location.href='#close';
         } else {
             $("#deleteDialog label").after("<div class='error'>That email is not correct.</div>");
@@ -239,27 +262,36 @@ $(function()
         return false;
     }
 
-    function layerInfo(layer, vURL, id, email) {
+    function layerInfo(layer, vURL, name, id, email, coordinates) {
         this.layer=layer;
         this.url = vURL;
+        this.name = name;
         this.id = id;
         this.email = email;
+        this.coordinates = coordinates;
     }
 
     function getSelectedLayerURL(layer) {
         for( var i in geoLayerCollection){
             var item = geoLayerCollection[i];
-            if(item.layer == layer){
+            if(item.layer == layer)
                 return item.url;
-            }
         }
     }
 
     function getSelectedLayerID(layer){
         for( var i in geoLayerCollection){
             var item = geoLayerCollection[i];
-            if(item.layer == layer){
+            if(item.layer == layer)
                 return item.id;
+        }
+    }
+
+    function getSelectedLayerName(layer){
+        for( var i in geoLayerCollection){
+            var item = geoLayerCollection[i];
+            if(item.layer == layer) {
+                return item.name;
             }
         }
     }
@@ -267,9 +299,16 @@ $(function()
     function getSelectedLayerEmail(layer){
         for( var i in geoLayerCollection){
             var item = geoLayerCollection[i];
-            if(item.layer == layer){
+            if(item.layer == layer)
                 return item.email;
-            }
+        }
+    }
+
+    function getSelectedLayerCoordinates(layer) {
+        for( var i in geoLayerCollection){
+            var item = geoLayerCollection[i];
+            if(item.layer == layer)
+                return item.coordinates;
         }
     }
 
